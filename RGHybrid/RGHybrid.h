a6226968fcd86ec50784ec0bca88d90bdb7e8971
TingtingYan <sunny@sunny.lan>
2018-02-24 01:09:17 +0800

    update gitignore

---
+++ RGHybrid/RGHybrid.h
@@ -0,0 +1,19 @@
+//
+//  RGHybrid.h
+//  RGHybrid
+//
+//  Created by TingtingYan on 2018/2/24.
+//  Copyright © 2018年 ranger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGHybrid.
+FOUNDATION_EXPORT double RGHybridVersionNumber;
+
+//! Project version string for RGHybrid.
+FOUNDATION_EXPORT const unsigned char RGHybridVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGHybrid/PublicHeader.h>
+
+
---
+++ RGHybridTests/RGHybridTests.m
@@ -0,0 +1,39 @@
+//
+//  RGHybridTests.m
+//  RGHybridTests
+//
+//  Created by TingtingYan on 2018/2/24.
+//  Copyright © 2018年 ranger. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RGHybridTests : XCTestCase
+
+@end
+
+@implementation RGHybridTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
